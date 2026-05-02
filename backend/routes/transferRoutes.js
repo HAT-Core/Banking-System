b5ddd12f46edadb11dccd0357ff58d17bcc87bf0
Muhammad Tayyab <tayyabkaramat423@gmail.com>
2026-05-02 18:27:20 +0500

    refactor: consolidate intra and inter transfers into transferController and align db pool initialization

--- backend/routes/transferRoutes.js
+++ backend/routes/transferRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const { intraTransfer } = require('../controllers/transferController');
+const { intraTransfer, interTransfer } = require('../controllers/transferController');
 
 router.post('/intra', protect, intraTransfer);
+router.post('/inter', protect, interTransfer);
 
 module.exports = router;
