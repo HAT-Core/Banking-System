334d711d40d3eaee7eeae625cdc4a4e5b4e44f83
Muhammad Tayyab <tayyabkaramat423@gmail.com>
2026-05-02 20:01:57 +0500

    chore: register /api/branch routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {connectDB} = require('./config/db');
 const transactionRoutes = require('./routes/transactionRoutes');
 const transferRoutes = require('./routes/transferRoutes');
+const branchRoutes = require('./routes/branchRoutes');
 
 const cors = require('cors');
 const app = express();
@@ -13,8 +14,11 @@ app.use('/api/auth',require('./routes/authRoutes'));
 app.use('/api/admin',require('./routes/adminRoutes'));
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/transfer', transferRoutes);
+app.use('/api/branch', branchRoutes);
 connectDB();
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
