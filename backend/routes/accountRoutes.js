d8e9798f20e090f1fdc4e0f6d1055a718ef97fbb
Muhammad Tayyab <tayyabkaramat423@gmail.com>
2026-05-03 17:29:46 +0500

    fix: remove duplicate import in accountRoutes.js

--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { getMyAccounts } = require('../controllers/accountController');
 const { getMyAccounts, getActiveBanks } = require('../controllers/accountController');
 
 router.get('/my', protect, getMyAccounts);
