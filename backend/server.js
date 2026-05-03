6f62d2c17df85e14311fbe04f15a12758c516c9c
Muhammad Tayyab <tayyabkaramat423@gmail.com>
2026-05-03 16:58:45 +0500

    chore: register /api/accounts routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,21 @@ const { runScheduledTransfers } = require('./jobs/scheduledTransferJob');
 const transactionRoutes = require('./routes/transactionRoutes');
 const transferRoutes = require('./routes/transferRoutes');
 const branchRoutes = require('./routes/branchRoutes');
+const accountRoutes = require('./routes/accountRoutes');
+const authRoutes = require('./routes/authRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/api/auth',require('./routes/authRoutes'));
-app.use('/api/admin',require('./routes/adminRoutes'));
+app.use('/api/auth',authRoutes);
+app.use('/api/admin',adminRoutes);
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/transfer', transferRoutes);
 app.use('/api/branch', branchRoutes);
+app.use('/api/accounts', accountRoutes);
 
 connectDB();
 runScheduledTransfers();
@@ -23,5 +27,7 @@ runScheduledTransfers();
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
 
 
