2fe18979032c5cba4de631bb7564f5632d345b2c
Muhammad Tayyab <tayyabkaramat423@gmail.com>
2026-05-05 07:59:09 +0500

    fix: restore old db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,23 +3,23 @@ const sql = require('mssql');
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: "127.0.0.1",
+    server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
-    options: {
-        trustServerCertificate: true,
+    options:{
         encrypt: false,
-        instanceName: "MSSQLSERVER02"
+        trustServerCertificate: true
     }
 };
 
-const connectDB = async () => {
-    try {
+const connectDB = async()=>{
+    try{
         await sql.connect(config);
         console.log("MSSQL Database connected successfully!");
-    } catch (err) {
+    }
+    catch(err){
         console.error("Database connection failed:", err);
         process.exit(1);
     }
 };
 
-module.exports = { sql, connectDB };
+module.exports = {sql, connectDB};
